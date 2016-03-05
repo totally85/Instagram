613723c95bb7b698804398a613f543e04dfb10b1
Allison Martin <totally85@Allisons-Mac.local>
2016-03-05 11:26:11 -0600

    Added logout

--- IG/ViewController.swift
+++ IG/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logoutButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func logoutPressed(sender: AnyObject)
+    {
+        PFUser.logOut()
+        print("User logged out")
+        
+        self.performSegueWithIdentifier("logoutSegue", sender: nil)
+    }
 
 }
 
